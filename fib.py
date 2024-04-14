a0e3d36e5f686df20883259b6170cfaf1b63ca1d
tmittal <tmittal@umich.edu>
2024-04-14 11:16:20 -0400

    added function def and function input

--- fib.py
+++ fib.py
@@ -1,4 +1,18 @@
-a, b = 1, 1
-while a < 10:
-    print(a)
-    a, b = b, a+b
+def main():  
+    num_terms = input("Enter number of Fibonacci terms: ")
+    valid = num_terms.isnumeric()
+
+    if valid:
+        num_terms = int(num_terms)
+        a, b = 1, 1
+        for i in range(0, num_terms):
+            print(a)
+            a, b = b, a+b
+            i += 1
+    else:
+        print("Invalid input. Input must be a numeric value.")
+
+
+if __name__ == "__main__":
+    main()
+
